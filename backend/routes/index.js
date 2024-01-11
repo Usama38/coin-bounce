5ddd18cbc091adbe69cc95b80e4cf3c256cf4f92
DESKTOP-AL5Q5N3\Usama <muhammadusama@tracko.com>
2024-01-11 11:32:18 +0500

    add comment controller with routes in backend code

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
-
-const authController = require("../controller/authController");
-
 const auth = require("../middlewares/auth");
+const authController = require("../controller/authController");
 const blogController = require("../controller/blogController");
-
+const commentController = require("../controller/commentController");
 const router = express.Router();
 
 //testing route
@@ -13,6 +11,7 @@ router.get("/test", (req, res) =>
   res.json({ Msg: "Working with test endpoint!" })
 );
 
+//auth controller
 router.post("/register", authController.register);
 
 router.post("/login", authController.login);
@@ -21,7 +20,7 @@ router.post("/logout", auth, authController.logout);
 
 router.get("/refresh", authController.refresh);
 
-//blog
+//blog controller
 //create
 router.post("/blog", auth, blogController.create);
 
@@ -37,4 +36,10 @@ router.put("/blog", auth, blogController.update);
 //delete
 router.delete("/blog/:id", auth, blogController.delete);
 
+//comment controller
+//create
+router.post("/comment", auth, commentController.create);
+
+//get
+router.get("/comment/:id", auth, commentController.getById);
 module.exports = router;
